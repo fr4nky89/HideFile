812d8e95de4051874bae72d30d8b2f614fa151be
fr4nky89 <frank.libolt@hotmail.com>
2013-07-10 15:00:01 +0200

    First commit

---
+++ HideFile/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Forms;
+namespace HideFile
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private string imagePath;
+        private string zipPath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FileButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.FileName = "Image"; // Default file name
+            ofd.DefaultExt = ".png"; // Default file extension
+            ofd.Filter = "Fichiers compressés (*.zip)|*.zip|Tous les fichiers (*.*)|*.*"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = ofd.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                FilePath.Text = ofd.FileName;
+            }
+        }
+
+        private void FolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = Environment.SpecialFolder.CommonPictures;
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                FolderPath.Text = fbd.SelectedPath;
+
+        }
+
+        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.FileName = "Image"; // Default file name
+            ofd.DefaultExt = ".png"; // Default file extension
+            ofd.Filter = "Images (*.png, *.jpg, *.jpeg, *.gif)|*.png; *.jpg; *.jpeg; *.gif|Tous les fichiers (*.*)|*.*"; // Filter files by extension
+            ofd.Multiselect = false;
+            ofd.Title = "Choisissez un fichier...";
+
+            // Show open file dialog box
+            Nullable<bool> result = ofd.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                ImagePath.Text = ofd.FileName;
+                string[] filename = ofd.FileName.Split('\\');
+                int i = filename.Length - 1;
+                ImageName.Text = filename[i] ;
+            }
+        }
+
+        private void ValidationButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
